1051be403f62b93909ed27e26fea334e7a47c0b0
Jade Farmer <JadeLewis4499@gmail.com>
2023-03-26 20:32:28 -0500

    Finished my sign up feature

--- app.js
+++ app.js
@@ -1,46 +1,73 @@
+/*Registration Form*/
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-$(document).ready(function(){
-    let bannerCount = 0;
-    setInterval(function(){
-        hideAllBanner();
-        changeBannerCount();
-        changeBanner();
-    }, 8000);
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-    function changeBanner(){
-        $('.banner-item').each(function(idx){
-            if(bannerCount == idx){
-                $(this).addClass('active-banner');
-            }
-        });
-    }
-
-    function hideAllBanner(){
-        $('.banner-item').each(function(idx){
-            $(this).removeClass('active-banner');
-        });
-    }
-
-    function changeBannerCount(){
-        bannerCount++;
-        if(bannerCount >= $('.banner-item').length){
-            bannerCount = 0;
-        }
-    }
-
-    // navigation bar toggle
-    $('#navbar-toggler').click(function(){
-        $('.navbar-collapse').slideToggle(600);
-    });
-
-    // fixed navbar 
-    $(window).scroll(function(){
-        let pos = $(window).scrollTop();
-        if(pos >= 800){
-            $('.navbar').addClass('fxd-navbar');
-        } else {
-            $('.navbar').removeClass('fxd-navbar');
-        }
-    });
-
+    validateInputs();
 });
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+};
